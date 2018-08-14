85742a103d754296271554f6c7e32f873eaba91b
Katrina Ariola <kat3ariola@gmail.com>
2018-08-14 12:45:19 +0900

    added services controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :services
+  # get 'services/index'
+  # get 'services/show'
+  # get 'services/new'
+  # get 'services/create'
+  # get 'services/edit'
+  # get 'services/update'
+  # get 'services/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
