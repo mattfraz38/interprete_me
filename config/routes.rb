1843b85b110fbf992ea51c93473196d378adc0df
George Walden <George.walden7@gmail.com>
2018-08-14 16:17:55 +0900

    Updated show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :services
 
   devise_for :users
-<<<<<<< HEAD
-=======
-
->>>>>>> 4460d6e1e8caefb6937fe30a89c25155bafb8f7c
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
