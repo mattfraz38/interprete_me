11a14b2ff7df0e1a02ff4b791a3e5618e09a2cf5
George Walden <George.walden7@gmail.com>
2018-08-14 13:11:15 +0900

    Added Appointments and User_services Controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: :show
-  resources :services
+  resources :services do
+    resources :appointments, only: :create
+  end
+
+  get "/my_services", to: 'user_services#index'
+
 end
