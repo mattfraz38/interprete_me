e7fcb1ec16a8772a02f5ca9a8badded33adf6994
George Walden <George.walden7@gmail.com>
2018-08-14 18:08:26 +0900

    Fixed Create Service

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -14,8 +14,9 @@ class ServicesController < ApplicationController
 
   def create
     @service = Service.new(service_params)
+    @service.user = current_user
     if @service.save
-      redirect_to my_services_path
+      redirect_to user_path(current_user)
     else
       render :new
     end
