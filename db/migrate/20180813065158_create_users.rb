496261b23422c6693e891f88371149b8332dbe12
George Walden <George.walden7@gmail.com>
2018-08-14 16:52:01 +0900

    Sign Up Page

--- db/migrate/20180813065158_create_users.rb
+++ db/migrate/20180813065158_create_users.rb
@@ -3,7 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.text :bio
-      t.text :photo
       t.string :gender
       t.text :languages
       t.string :availability
