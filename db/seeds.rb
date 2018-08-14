9b1b004f141041180a79b13f1bb6536ad0d96ae1
George Walden <George.walden7@gmail.com>
2018-08-14 12:30:44 +0900

    Updated Controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,38 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Appointment.delete_all
+Service.delete_all
+
+User.delete_all
+
+puts "Deleted all items!"
 
 User.create!(email: "larrysworld@gmail.com", password: "IloveMatt", name: "Larry the Hairy Fairy", bio: "Loves puppies, and flying high in the sky. Lactose intolerant. Open minded. Matt call me back, please.", gender: "Male", languages: ["English", "German", "Spanish", "Italian", "French", "Finnish", "Swedish"])
 User.create!(email: "mattattack@gmail.com", password: "Pokemon123", name: "Matt the Big Lat", bio: "Loves his protein shakes and heavy weights.", gender: "Male", languages: ["English", "Japanese"])
 User.create!(email: "kitkat@gmail.com", password: "Whatupppppp", name: "Kat with the Nice Tat", bio: "Fight me.", gender: "Female", languages: ["English", "Tagalog"])
 User.create!(email: "georgewss@gmail.com", password: "HelloWorld", name: "George ", bio: "", gender: "Male", languages: ["English"])
 
-Service.create!(category: "Night Life", title: "Club!", description: "Go to the club", price: 20, location: "Tokyo", user_id: 1, language: "German")
-Service.create!(category: "Day Life", title: "Swim with the sharks", description: "Go swimming with the sharks!", price: 23030, location: "Tokyo", user_id: 3, language: "Japanese")
-Service.create!(category: "Adventure", title: "Dive in the ocean", description: "Dive down deep", price: 900, location: "Tokyo", user_id: 2, language: "English")
-Service.create!(category: "Sports", title: "Play baseball", description: "Play with the Giants!", price: 15, location: "Tokyo", user_id: 1, language: "Japanese")
-Service.create!(category: "Sports", title: "See soccer", description: "See Honda", price: 10, location: "Tokyo", user_id: 4, language: "Japanese")
-Service.create!(category: "Food", title: "Wine tasting", description: "Drink. A lot.", price: 11000, location: "Tokyo", user_id: 4, language: "English")
-Service.create!(category: "Relax", title: "Spa", description: "Relax", price: 4, location: "Tokyo", user_id: 2, language: "Japanese")
-Service.create!(category: "Adventure", title: "Fishing", description: "Catch the big one", price: 50, location: "Tokyo", user_id: 3, language: "Tagalog")
+puts "Created #{User.count} Users!"
 
-Appointment.create!(start_day: (Date.today + 10), end_day: (Date.today + 15), confirmed: false, user_id: 2, service_id: 5)
-Appointment.create!(start_day: (Date.today + 20), end_day: (Date.today + 25), confirmed: false, user_id: 4, service_id: 2)
-Appointment.create!(start_day: (Date.today), end_day: (Date.today + 15), confirmed: false, user_id: 3, service_id: 3)
-Appointment.create!(start_day: (Date.today + 2), end_day: (Date.today + 30), confirmed: false, user_id: 1, service_id: 6)
-Appointment.create!(start_day: (Date.today + 10), end_day: (Date.today + 15), confirmed: false, user_id: 3, service_id: 3)
-Appointment.create!(start_day: (Date.today + 20), end_day: (Date.today + 25), confirmed: false, user_id: 1, service_id: 4)
-Appointment.create!(start_day: (Date.today), end_day: (Date.today + 15), confirmed: false, user_id: 4, service_id: 1)
-Appointment.create!(start_day: (Date.today + 2), end_day: (Date.today + 30), confirmed: false, user_id: 2, service_id: 2)
+Service.create!(category: "Night Life", title: "Club!", description: "Go to the club", price: 20, location: "Tokyo", user_id: User.all.sample.id, language: "German")
+Service.create!(category: "Day Life", title: "Swim with the sharks", description: "Go swimming with the sharks!", price: 23030, location: "Tokyo", user_id: User.all.sample.id, language: "Japanese")
+Service.create!(category: "Adventure", title: "Dive in the ocean", description: "Dive down deep", price: 900, location: "Tokyo", user_id: User.all.sample.id, language: "English")
+Service.create!(category: "Sports", title: "Play baseball", description: "Play with the Giants!", price: 15, location: "Tokyo", user_id: User.all.sample.id, language: "Japanese")
+Service.create!(category: "Sports", title: "See soccer", description: "See Honda", price: 10, location: "Tokyo", user_id: User.all.sample.id, language: "Japanese")
+Service.create!(category: "Food", title: "Wine tasting", description: "Drink. A lot.", price: 11000, location: "Tokyo", user_id: User.all.sample.id, language: "English")
+Service.create!(category: "Relax", title: "Spa", description: "Relax", price: 4, location: "Tokyo", user_id: User.all.sample.id, language: "Japanese")
+Service.create!(category: "Adventure", title: "Fishing", description: "Catch the big one", price: 50, location: "Tokyo", user_id: User.all.sample.id, language: "Tagalog")
+
+puts "Created #{Service.count} Services!"
+
+Appointment.create!(start_day: (Date.today + 10), end_day: (Date.today + 15), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today + 20), end_day: (Date.today + 25), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today), end_day: (Date.today + 15), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today + 2), end_day: (Date.today + 30), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today + 10), end_day: (Date.today + 15), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today + 20), end_day: (Date.today + 25), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today), end_day: (Date.today + 15), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+Appointment.create!(start_day: (Date.today + 2), end_day: (Date.today + 30), confirmed: false, user_id: User.all.sample.id, service_id: Service.all.sample.id)
+
+puts "Created #{Appointment.count} Appointments!"
