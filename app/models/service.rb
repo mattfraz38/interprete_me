c37da025e5f8f83cf6e8d4e22f3b8a760fbd395f
Katrina Ariola <kat3ariola@gmail.com>
2018-08-16 13:48:24 +0900

    add search function

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,13 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :appointments
   validates :user, :description, :price, :location, :title, :category, presence: :true
+
+  include PgSearch
+  pg_search_scope :search_by_title_desc_cat,
+    against: [ :title, :description, :category ],
+  using: {
+    tsearch: { prefix: true }
+  }
+
+
 end
