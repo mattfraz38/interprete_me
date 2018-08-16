408cff55d502214c5c4907baac441a874bfd0d00
Larry Nevins <ichbinlarry@gmail.com>
2018-08-16 13:54:39 +0900

    User page modified new color

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo, :photo_cache])
 
     # For additional in app/views/devise/registrations/edit.html.erb
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :photo])
+
   end
 
   private
